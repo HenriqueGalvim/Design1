67e0f9a9c3f5b44af50c895634e94a6fea7c15cc
HenriqueGalvim <64918330+HenriqueGalvim@users.noreply.github.com>
2020-09-06 13:02:20 -0300

    Commit final

--- main.js
+++ main.js
@@ -1,26 +1,60 @@
- 	
+  	
   //variaveis globais
  	let nome= document.getElementById("Primeiro-nome")
  	let secNome= document.getElementById("Segundo-nome")
  	let email= document.getElementById("email")
- 	const senha= document.getElementById("senha")
+ 	let senha= document.getElementById("senha")
+
+ 	let svg= document.getElementById("svg")
+ 	let svg2= document.getElementById("svg2")
+ 	let svg3= document.getElementById("svg3")
+ 	let svg4= document.getElementById("svg4")
+
+ 	let res1= document.getElementById("res1")
+ 	let res2= document.getElementById("res2")
+ 	let res3= document.getElementById("res3")
+ 	let res4= document.getElementById("res4")
+
 	 function clicar(){  //função de clicar
 
 	 	if(nome.value.length == 0){ // If do Nome
-			window.alert("Preecha os dados corretamente")
-			}
+	 		res1.innerHTML="<p>First name cannot be empty</p>"
+	 		nome.style.border="2px solid red"
+	 		svg.style.display="inline-block"
+	 	} else{
+	 		res1.innerHTML=""
+	 		nome.style.border="2px solid green"
+	 		svg.style.display="none"
+	 	}	
+			
 		if(secNome.value.length == 0){ // If do sobrenome
-			window.alert("Preecha os dados corretamente")
-			 
-		}	
+			res2.innerHTML="<p>Last name cannot be empty</p>"
+			secNome.style.border="2px solid red" 
+	 		svg2.style.display="inline-block"
+		}else{
+	 		res2.innerHTML=""
+	 		secNome.style.border="2px solid green"
+	 		svg2.style.display="none"
+	 	}	
 
 		if (email.value.length == 0) { // If do email
-			window.alert('Email invalido, use o gmail de preferencia');	
-			
-		}
+		    res3.innerHTML="<p>   invalid email</p>"
+		    email.style.border="2px solid red"
+	 		svg3.style.display="inline-block"
+		}else{
+	 		res3.innerHTML=""
+	 		email.style.border="2px solid green"
+	 		svg3.style.display="none"
+	 	}
 
-		if (senha.value.length <=6 ) {
-			window.alert("Senha curta demais, insira uma de no minimo 6 caracteres")
-			
-	 }
+		if (senha.value.length < 6 ) {
+			res4.innerHTML="<p>    6-digit passwords only</p>"
+			senha.style.border="2px solid red"
+	 		svg4.style.display="inline-block"
+	     }else{
+	 		res4.innerHTML=""
+	 		senha.style.border="2px solid green"
+	 		svg4.style.display="none"
+	 	}
+
 } 
